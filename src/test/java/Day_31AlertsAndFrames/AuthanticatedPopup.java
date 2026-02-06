5b8ad0a5b7960b72e27c11cab3beaddbe9f992e9
NuruddinSyed18 <nuruddinsyed18@gmail.com>
2026-02-06 18:42:21 +0530

    Learning automation testing - 6th feb

---
+++ src/test/java/Day_31AlertsAndFrames/AuthanticatedPopup.java
@@ -0,0 +1,25 @@
+package Day_31AlertsAndFrames;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AuthanticatedPopup {
+public static void main(String[] args) {
+		
+		
+		WebDriver driver=new ChromeDriver();
+		
+		//driver.get("http://the-internet.herokuapp.com/basic_auth");
+		
+		//syntax
+		//http://username:password@the-internet.herokuapp.com/basic_auth
+			
+		driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+			
+		driver.manage().window().maximize();
+
+
+	}
+
+
+}
